d36d7c38cdad033d0c1d778d82e381cd051a2823
damienfamed75 <thedoubledimensions@gmail.com>
2022-09-06 23:01:31 -0500

    Change modelpath for pistolAmmo

--- code/weapons/PistolAmmo.cs
+++ code/weapons/PistolAmmo.cs
@@ -1,15 +1,14 @@
 using System;
-using System.Xml.Schema;
 using Sandbox;
 using SandboxEditor;
 
 [Library("ammo_pistol"), HammerEntity]
 [Title("Pistol Ammo"), Category("Ammo")]
-[EditorModel("models/sbox_props/burger_box/burger_box.vmdl")]
+[EditorModel("models/rust_props/small_junk/tea_box.vmdl")]
 public class PistolAmmo : Ammunition
 {
 	protected override Type WeaponType { get; set; } = typeof( Pistol );
 	protected override int AmmoAmount { get; set; } = 25;
-	protected override string ModelPath { get; set; } = "models/sbox_props/burger_box/burger_box.vmdl";
+	protected override string ModelPath { get; set; } = "models/rust_props/small_junk/tea_box.vmdl";
 	protected override string SoundPath { get; set; } = "pistol_ammo_pickup";
 }
